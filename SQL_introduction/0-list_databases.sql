c597d913129b2d21abc23544b946c93edd460899
Hiskiyas <hiskiyasf@gmail.com>
2023-08-16 12:03:15 +0300

    nEW ATTEMPT

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -2,4 +2,6 @@
 -- because Batch 3 is the best!
 SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
 CREATE USER 'hiskiyas'@'localhost';
-SHOW DATABASES;
+SHOW -- Get a list of databases
+SELECT name FROM sys.databases
+GO
