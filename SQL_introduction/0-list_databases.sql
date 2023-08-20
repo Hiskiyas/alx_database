55a2b6d3dd1166fc490e3ae81a33963b191c9224
Hiskiyas <hiskiyasf@gmail.com>
2023-08-20 22:00:05 +0300

    show Database

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,7 +1,2 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
-SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
-CREATE USER 'hiskiyas'@'localhost';
-SHOW -- Get a list of databases
-SELECT name FROM sys.databases
-GO
+-- this displays all databases
+SHOW DATABASES;
